4437c321f70456fd291c3395366f88f5953666de
lucas.arrabal_uscson <lucas.arrabal_uscson@PC-06-30>
2022-10-27 10:32:37 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        
+        [HttpPost]
+        public void AdicionarFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O campo titulo é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage ="O genero nao pode passar de 30 letras")]
+        public string Genero { get; set; }
+        [Range(1,600 , ErrorMessage = "O filme deve ter entre 1 a 600 min")]
+        public int Duracao { get; set; }
+
+    }
+}
